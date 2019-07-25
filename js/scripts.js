129fa14309468a70c2a8ec2efefb51eed5931227
mireille <mimimireille95@gmail.com>
2019-07-25 13:51:14 -0400

    new line

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,16 @@
     
     });
   });
+  $(function(){
+      $('#image1').hover(function(){
+          $(this).width(1.25 * $(this).width());
+          $(this).height(1.25 * $(this).height());
+      },
+      function(){
+        $(this).width(0.8 * $(this).width());
+        $(this).height(0.8 * $(this).height());
+      });
+  });
 //   $(document).ready(function() {
 //       $("#a").click(function() {
 //         $(".walrus-showing").toggle();
